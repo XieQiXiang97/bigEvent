deba7b4f5bdf5b3a57e10cf5064fc71f82cc57b3
xieqixiang97 <898359491@qq.com>
2021-03-01 14:39:01 +0800

    修改了login

--- assets/js/login.js
+++ assets/js/login.js
@@ -6,6 +6,33 @@ $(function () {
     $('#link-login').on('click', function () {
         $('.login-box').show()
         $('.reg-box').hide()
+    });
+
+    var form = layui.form
+    var layer = layui.layer
+    form.verify({
+        // 自定义了一个叫做 pwd 校验规则
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        // 校验两次密码是否一致的规则
+        repwd: function (value) {
+            // 通过形参拿到的是确认密码框中的内容
+            // 还需要拿到密码框中的内容
+            // 然后进行一次等于的判断
+            // 如果判断失败,则return一个提示消息即可
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return '两次密码不一致！'
+            }
+        }
+    })
+    $('#form-reg').on('submit', function (e) {
+        // 1. 阻止默认的提交行为
+        e.preventDefault()
+        // 2. 发起Ajax的POST请求
+        var data = {
+            username: $('#form-reg [name=username]').val(),
+            password: $('#form-reg [name=password]').val()
+        }
         $.ajax({
             type: "post",
             url: "/api/reguser",
@@ -14,13 +41,12 @@ $(function () {
                 if (res.status !== 0) {
                     return layer.msg(res.message)
                 }
-                console.log(123);
                 layer.msg('注册成功，请登录！')
                 // 模拟人的点击行为
                 $('#link-login').click()
             }
-        })
-    });
+        });
+    })
     $('#form-login').on('sunmit', function () {
         $.ajax({
             type: "post",
